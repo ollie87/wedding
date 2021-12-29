6501e3540214e2c745fd22ed09d27724baacbc9d
mgarridov <mgarridov@minsait.com>
2021-12-29 11:41:35 +0100

    feat(atoms): style form components added

---
+++ components/atoms/input-submit.jsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+export const InputSubmit = styled.input`
+    background-color: var(--naranja);
+    width: 100%;
+    padding: 1.5rem;
+    text-align: center;
+    color: #FFF;
+    font-size: 1.8rem;
+    text-transform: uppercase;
+    border: none;
+    font-family: 'PT Sans', sans-serif;
+    font-weight: 700;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
