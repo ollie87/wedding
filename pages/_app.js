6b08ef6f32f373d27ee247b912f3bd978ca402b2
mgarridov <mgarridov@minsait.com>
2021-12-31 13:56:37 +0100

    feat(package): next-i18next removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import firebase, { FirebaseContext } from '../firebase';
 import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
-import { appWithTranslation } from 'next-i18next';
 
 const App = ({ Component, pageProps }) => {
   return (
@@ -17,4 +16,4 @@ const App = ({ Component, pageProps }) => {
   )
 }
 
-export default appWithTranslation(App)
+export default App
