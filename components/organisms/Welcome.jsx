fa61d5506f89415813dc081e952cfee2016a9b02
mgarridov <mgarridov@minsait.com>
2022-01-09 14:46:00 +0100

    feat(Welcome): text formatted

--- components/organisms/Welcome.jsx
+++ components/organisms/Welcome.jsx
@@ -7,10 +7,11 @@ export default function Welcome() {
             <div className="flex flex-col 2xl:w-7/12 md:w-full items-center">
                 <Title1>!Bienvenidos a nuestra boda!</Title1>
                 <Paragraph>
-                    ¡Que sí! Que nos casamos (ya era hora)!!!																			
-                    ¡Estamos super felices! Nos sentimos en las nubes y queremos compartir este día tan especial. Por eso estamos preparando una boda que hará historia y en la que te lo pasarás genial.
-                    Mientras llega el gran día hemos creado esta web con un montón de secciones para que estés al día de todo y para compartir nuestra historia de amor.
-                    Una cosa importante, en la sección asistencia puedes confirmar si vas a la boda o no, intolerancias, alergias o dieta especial (vegetariana o vegana). Confírmanos lo antes posible por favor, que así organizarlo todo nos será mucho más fácil.                                                                                
+                    ¡Que sí! Que nos casamos<br />
+                    (ya era hora)!!!<br /><br />															
+                    ¡Estamos super felices! Nos sentimos en las nubes y queremos compartir este día tan especial. Por eso estamos preparando una boda que hará historia y en la que te lo pasarás genial.<br /><br />
+                    Mientras llega el gran día hemos creado esta web con un montón de secciones para que estés al día de todo y para compartir nuestra historia de amor.<br /><br />
+                    Una cosa importante, en la sección asistencia puedes confirmar si vas a la boda o no, intolerancias, alergias o dieta especial (vegetariana o vegana). Confírmanos lo antes posible por favor, que así organizarlo todo nos será mucho más fácil.<br /><br />
                     Disfruta la web y nos vemos muy pronto, mil besos!
                 </Paragraph>
             </div>
