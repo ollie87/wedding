3e2059237fb485b29c25dbf14d302384560a49db
mgarridov <mgarridov@minsait.com>
2022-01-29 21:26:24 +0100

    feat(headerStyles): position adjust

--- components/atoms/headerStyles.jsx
+++ components/atoms/headerStyles.jsx
@@ -4,8 +4,9 @@ export const HeaderStyle = styled.div`
     width: 100%;
     height: 500px;
     background: url("/img/headerbg.jpg");
-    background-position: center;
+    background-position-x: center;
     background-size: 100% auto;
+    background-position-y: 75%;
     div {
         width: 100%;
         backdrop-filter: blur(4px);
