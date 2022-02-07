29144b056cdbbf74646ae4f4e5aabdf2be423b29
mgarridov <mgarridov@minsait.com>
2022-02-07 18:22:21 +0100

    feat(road-map-style): media added

--- components/atoms/road-map-style.jsx
+++ components/atoms/road-map-style.jsx
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const RoadMapStyle = styled.div`
     width: 100%;
     padding-bottom: 10rem;
+
+    @media (max-width: 768px) {
+        padding-bottom: 5rem;
+    }
 `
