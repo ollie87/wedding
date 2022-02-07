f5416e353fafcf03fabc900b76f21fa8d3302b80
mgarridov <mgarridov@minsait.com>
2022-02-07 18:45:22 +0100

    feat(road-map-text-time): fix error

--- components/atoms/road-map-text-time.jsx
+++ components/atoms/road-map-text-time.jsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
 export const RoadMapTextTime = styled.h3`
-    font-size: 2rem;
+    font-size: 3rem;
     color:  ${({ theme: { colors } }) => colors.secondary};
     margin: 0;
     line-height: 0.8;
+    @media (max-width: 768px) {
+        font-size: 2rem;
+    }
 ` 
